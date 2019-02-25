b93bc754c075f62d537ed445ab240450f2b48f35
jose suero <jsuero@mstn.com>
2019-02-24 21:30:53 -0400

    separate credit card and input

--- components/Input.js
+++ components/Input.js
@@ -3,12 +3,21 @@ import { View, TextInput, StyleSheet, Text } from 'react-native';
 import { TextInputMask } from 'react-native-masked-text';
 import Stripe from 'react-native-stripe-api';
 
+import PropTypes from 'prop-types';
+
 import Icon from './Icon';
 import { Row, Column } from './Table';
 import properties from './properties';
 
 
 export default class Input extends Component {
+    currentOnChange = (value) => {
+        this.props.onChange(value)
+    }
+    onChange(value) {
+
+    }
+
     render() {
         let multiline = false;
         let { inputStyle, inputBoxStyle } = styles;
@@ -57,223 +66,30 @@ export default class Input extends Component {
         );
     };
 }
-export class CreditCard extends React.Component {
-    state = {
-        ready: false,
-        stripeError: false,
-        numberError: false,
-        expError: false,
-        cvcError: false,
-        error: "",
-        number: "",
-        exp: "",
-        cvc: "",
-        token: {},
-    }
-    constructor() {
-        super();
-        this.stripe = new Stripe(properties.stripeApiKey);
-    }
 
-    componentWillMount() {
-        if (this.props.value) {
-            this.setState({ ...this.props.value });
-        }
-    }
+Input.propTypes = {
+    onChange: PropTypes.func.isRequired,
+    value: PropTypes.string.isRequired,
 
-    setError(code, order) {
-        let { error, stripeError, numberError,
-            expError,
-            cvcError,
-        } = this.state;
-        switch (code) {
-            case "incorrect_number":
-                stripeError = true;
-                error = "Número de tarjeta inválido";
-                break;
-            case "EXP_NOT_VALID":
-                if (this.state.exp.length > 0) {
-                    error = "Fecha expiración inválida";
-                }
-                expError = true;
-                this.exp.getElement().focus();
-                break;
-        }
-        this.setState({
-            stripeError,
-            numberError,
-            expError,
-            cvcError,
-            token: {},
-            error
-        });
-    }
+    multiline: PropTypes.bool,
+    placeholder: PropTypes.string,
+    secureTextEntry: PropTypes.bool,
+    type: PropTypes.string,
+    autoCapitalize: PropTypes.bool,
+    inputRef: PropTypes.func,
+    editable: PropTypes.bool,
+    blurOnSubmit: PropTypes.bool,
 
-    numberIsValid() {
-        return this.number.isValid() && this.state.number.length > 0
-    }
+    containerStyle: PropTypes.object,
+    style: PropTypes.object,
 
-    getToken() {
-        const exp = this.state.exp.split("/");
-        this.stripe.createToken({ number: this.state.number, exp_month: exp[0], exp_year: exp[1], cvc: this.state.cvc }).then(token => {
-            if (token.error) {
-                this.setError(token.error.code, 5);
-            } else {
-                this.setState({
-                    ready: true,
-                    token,
-                    stripeError: false
-                }, () => {
-                    this.onChange();
-                });
-            }
-        });
-    }
+    error: PropTypes.bool,
 
-    verify() {
-        if (!this.numberIsValid()) {
-            this.number.getElement().focus();
-        } else if (!this.exp.isValid()) {
-            this.setError("EXP_NOT_VALID", 1);
-        } else if (!this.cvc.isValid()) {
-            this.cvc.getElement().focus();
-        } else {
-            this.getToken();
-        }
-    }
-
-    onChange() {
-        if (this.props.onChange) {
-            this.props.onChange(this.state);
-        }
-    }
-    render() {
-        let borderColor = properties.borderColor;
-        if (this.props.error) {
-            borderColor = "red"
-        }
-
-        let { stripeError, numberError, expError, cvcError, error } = this.state;
-        if (!numberError && !expError && !cvcError && !stripeError) {
-            error = "";
-        }
-        return (<Column style={{
-            width: "100%",
-        }}>
-            <Row style={{
-                "borderWidth": 1,
-                "borderColor": borderColor,
-                "borderRadius": 5,
-                "backgroundColor": "white",
-                paddingTop: 5,
-                paddingRight: 10,
-                paddingBottom: 5,
-                paddingLeft: 0,
-                ...this.props.style
-            }}>
-                <Column style={{ flex: 2 }}>
-                    <TextInputMask
-                        ref={(ref) => this.number = ref}
-                        type={'credit-card'}
-                        keyboardType="number-pad"
-                        placeholder={'Numero de tarjeta'}
-                        options={{
-                            obfuscated: this.state.ready
-                        }}
-                        value={this.state.number}
-                        onChangeText={(item) => {
-                            this.setState({ number: this.number.getRawValue().join(""), ready: false }, () => {
-                                this.onChange();
-                                if (this.numberIsValid()) {
-                                    if (!this.exp.isValid()) {
-                                        this.setError("EXP_NOT_VALID", 2);
-                                    } else if (!this.cvc.isValid()) {
-                                        this.cvc.getElement().focus();
-                                    } else {
-                                        this.verify();
-                                    }
-                                }
-                            }
-                            );
-                        }}
-                    />
-                </Column>
-                <Column style={{ flex: 0.5 }}>
-                    <TextInputMask
-                        ref={(ref) => this.exp = ref}
-                        type={'custom'}
-                        keyboardType="number-pad"
-                        placeholder="MM/AA"
-                        value={this.state.exp}
-                        onChangeText={(item) => {
-                            this.setState({ exp: this.exp.getRawValue(), ready: false }, () => {
-                                this.onChange();
-                                if (this.exp.isValid()) {
-                                    this.setState({ expError: false }, () => {
-                                        if (!this.cvc.isValid()) {
-                                            this.cvc.getElement().focus();
-                                        } else if (!this.numberIsValid()) {
-                                            this.number.getElement().focus();
-                                        } else {
-                                            this.verify();
-                                        }
-                                    })
-                                }
-                            });
-                        }}
-                        options={{
-                            validator: ((value, settings) => {
-                                const parts = value.split('/');
-                                const result = (parts.length > 1 && parseInt(parts[0]) < 13 && parseInt(parts[1]) > 17);
-                                if (!result && value.length === 5) {
-                                    this.setError("EXP_NOT_VALID", 3);
-                                }
-                                return result;
-                            }).bind(this),
-                            mask: '99/99'
-                        }}
-                    />
-
-                </Column>
-                <Column style={{ flex: 0.5 }}>
-                    <TextInputMask
-                        ref={(ref) => this.cvc = ref}
-                        type={'custom'}
-                        keyboardType="number-pad"
-                        placeholder="CVC"
-                        value={this.state.cvc}
-                        onChangeText={(item) => {
-                            this.setState({ cvc: this.cvc.getRawValue(), ready: false });
-                            this.onChange();
-                            if (this.cvc.isValid()) {
-                                if (!this.numberIsValid()) {
-                                    this.number.getElement().focus();
-                                } else if (!this.exp.isValid()) {
-                                    this.setError("EXP_NOT_VALID", 4);
-                                } else {
-                                    this.verify();
-                                }
-                            }
-                        }}
-                        options={{
-                            validator: function (value, settings) {
-                                return value.length === 3
-                            },
-                            mask: '999'
-                        }}
-                    />
-                </Column>
-            </Row>
-            <Row style={{ justifyContent: "center" }}>
-                <Text style={{
-                    color: "red",
-                    fontSize: 10,
-                }}>{error}</Text>
-            </Row>
-        </Column>)
-    }
+    icon: PropTypes.string,
+    iconType: PropTypes.string,
+    iconStyle: PropTypes.object,
+    iconSize: PropTypes.number,
 }
-
 
 const styles = StyleSheet.create({
     ...properties.styles,
